29ebed569f18c6097a3d3d57ca1d0ef23e506618
Niv BP <nivbp7@gmail.com>
2024-12-31 16:52:46 +0200

    Add extension

--- IBI/Extensions/UIView+Utils.swift
+++ IBI/Extensions/UIView+Utils.swift
@@ -13,4 +13,19 @@ extension UIView {
             self.addSubview(view)
         }
     }
+    
+    func snap(to view: UIView, shouldAddToView: Bool = true) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if shouldAddToView {
+            view.addSubview(self)
+        }
+        
+        NSLayoutConstraint.activate([
+            self.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            self.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            self.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
