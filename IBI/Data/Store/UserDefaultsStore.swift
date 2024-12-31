164266e6a464736409d8c6910d1389ac79ce0023
Niv BP <nivbp7@gmail.com>
2024-12-31 18:00:35 +0200

    Add UserDefaultsStore

---
+++ IBI/Data/Store/UserDefaultsStore.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefaultsStore.swift
+//  IBI
+//
+//  Created by niv ben-porath on 31/12/2024.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<Value> {
+    let key: String
+    let defaultValue: Value
+    var container: UserDefaults = .standard
+
+    var wrappedValue: Value {
+        get {
+            return container.object(forKey: key) as? Value ?? defaultValue
+        }
+        set {
+            container.set(newValue, forKey: key)
+        }
+    }
+}
+
+extension UserDefaults {
+    @UserDefault(key: "isLoggedIn", defaultValue: false)
+    static var isLoggedIn: Bool
+}
