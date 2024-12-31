fc370238315bfe2d9e9708b48c0d69c05246368b
Niv BP <nivbp7@gmail.com>
2024-12-31 12:09:46 +0200

    Setup programmatic layout

--- IBI/SceneDelegate.swift
+++ IBI/SceneDelegate.swift
@@ -11,12 +11,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds) //fill the screen
+        window?.windowScene = windowScene
+        //set the root VC we want to show
+        window?.rootViewController = createMainNavigationController()
+        window?.makeKeyAndVisible()
+    }
+    
+    private func createMainNavigationController() -> UINavigationController {
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.setNavigationBarHidden(true, animated: false)
+        return navigationController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +54,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
