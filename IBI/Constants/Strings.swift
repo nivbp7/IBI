d5841e0bf93c1017fcdd8c89eeb756ed25027646
Niv BP <nivbp7@gmail.com>
2025-01-01 14:12:33 +0200

    Add Strings file

---
+++ IBI/Constants/Strings.swift
@@ -0,0 +1,13 @@
+//
+//  Strings.swift
+//  IBI
+//
+//  Created by niv ben-porath on 01/01/2025.
+//
+
+import Foundation
+
+struct Strings {
+    static let username = "IBI"
+    static let password = "password"
+}
