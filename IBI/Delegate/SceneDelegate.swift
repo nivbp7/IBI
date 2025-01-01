afadde8ec62ad8988511abc175bf3c998388dd2c
Niv BP <nivbp7@gmail.com>
2025-01-01 17:32:30 +0200

    Set display mode of window

--- IBI/Delegate/SceneDelegate.swift
+++ IBI/Delegate/SceneDelegate.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? {
+        didSet {
+            let appearance = Appearance(rawValue: UserDefaults.applicationAppearance) ?? Appearance.defaultAppearance
+            window?.overrideUserInterfaceStyle = appearance.userInterfaceStyle
+        }
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
